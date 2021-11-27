924cd036129bb3d284206ad1aa057512b42ae2b5
nevesfelipe47 <santosfelpe47@gmail.com>
2021-11-27 15:25:45 -0300

    Estudos de logica em C- primeiro programa1

---
+++ programa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+
+    int idade;
+    double salario, altura;
+    char genero;
+    char nome[50];
+
+    idade = 20;
+    salario = 5800.5;
+    altura = 1.63;
+    genero = 'F';
+    strcpy(nome, "Maria Silva");
+
+    printf("idade = %d\n",idade);
+    printf("salario= %.2lf\n",salario);
+    printf("altura = %.2lf\n",altura);
+    printf("genero = %c\n",genero);
+    printf("nome = %s\n",nome);
+}
