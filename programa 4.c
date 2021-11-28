0f4d4bad0a1232517ce61a0c599c296a53ec55fa
nevesfelipe47 <santosfelpe47@gmail.com>
2021-11-28 05:05:36 -0300

    Estudos de logica em C- primeiro programa4

---
+++ programa 4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+
+int idade;
+char nome[30] ;
+double altura;
+double peso,imc;
+
+    printf("Bem vindo ao calculo de imc\n");
+    printf("------------------------------\n");
+    printf("\n");
+    printf("Digite seu nome: \n");
+    scanf("%s",nome);
+    printf("Digite sua idade: \n");
+    scanf("%d",idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%lf",altura);
+    printf("Digite seu peso: \n");
+    scanf("%lf",peso);
+
+    imc = peso/(altura*altura);
+
+    printf("Seu IMC = %.2lf\n",imc);
+
+
+
+
+
+
+
+}
